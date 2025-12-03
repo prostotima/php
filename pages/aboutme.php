1bbe38de893d9d6614042c7ba9cc8b1d8f9e9ee4
 Aquarius <aquarius@Mac-mini-Aquarius.local>
2025-12-03 09:54:47 +0200

    auto

---
+++ pages/aboutme.php
@@ -0,0 +1,33 @@
+<?php
+
+use Classes\AboutMeController;
+
+$data = AboutMeController::show();
+
+echo $viewer->render('aboutme', $data);
+
+namespace Classes;
+
+/**
+ * Class AboutMeController
+ *
+ * Контролер для сторінки "About Me".
+ * Передає дані в шаблон aboutme.latte
+ */
+class AboutMeController
+{
+    /**
+     * Повертає масив даних для шаблону
+     *
+     * @return array
+     */
+    public static function show(): array
+    {
+        return [
+            'title' => 'About Me',
+            'name' => 'Tima',
+            'skills' => ['PHP', 'JS', 'MySQL', 'Git'],
+            'hobby' => 'Створення сайтів та програм'
+        ];
+    }
+}
